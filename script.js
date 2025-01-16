beb757194980d7d8c97919c5939b2cba756f8276
white-king1 <kelechivctorn@gmail.com>
2025-01-16 14:17:11 -0500

    new update done

--- script.js
+++ script.js
@@ -50,30 +50,30 @@ function slideToIndex(nextIndex) {
 initSlider();
 
 // Bonus for automatic slider starts here
-const navContainer = document.querySelector(".nav-container");
-let id;
-let isSliding = false;
+// const navContainer = document.querySelector(".nav-container");
+// let id;
+// let isSliding = false;
 
-function startAutoSlide() {
-  if (!isSliding) {
-    id = setInterval(() => slideToIndex(currIndex + 1), 2000);
-    isSliding = true;
-  }
-}
+// function startAutoSlide() {
+//   if (!isSliding) {
+//     id = setInterval(() => slideToIndex(currIndex + 1), 2000);
+//     isSliding = true;
+//   }
+// }
 
-function stopAutoSlide() {
-  clearInterval(id);
-  isSliding = false;
-}
+// function stopAutoSlide() {
+//   clearInterval(id);
+//   isSliding = false;
+// }
 
-startAutoSlide();
+// startAutoSlide();
 
-// STOP AUTOMATIC SLIDER WHEN USER HOVERS ON THE PAGE STARTS HERE
-navContainer.addEventListener("mouseover", () => stopAutoSlide());
-navContainer.addEventListener("mouseout", () => startAutoSlide());
+// // STOP AUTOMATIC SLIDER WHEN USER HOVERS ON THE PAGE STARTS HERE
+// navContainer.addEventListener("mouseover", () => stopAutoSlide());
+// navContainer.addEventListener("mouseout", () => startAutoSlide());
 
-document.addEventListener("touchstart", (e) => {
-  if (e.target !== navContainer && !navContainer.contains(e.target))
-    startAutoSlide();
-});
+// document.addEventListener("touchstart", (e) => {
+//   if (e.target !== navContainer && !navContainer.contains(e.target))
+//     startAutoSlide();
+// });
 // STOP AUTOMATIC SLIDER WHEN USER HOVERS ON THE PAGE ENDS HERE
